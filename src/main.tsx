0b17e75ba1a5ba65206f58003f89788116402358
Azafman <0000880867@senaimgaluno.com.br>
2024-09-20 01:25:39 -0300

    Setup projeto - styled-components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {App} from './App.tsx'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/Themes/default.ts'
+import { GlobalStyle } from './styles/Global.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+      <GlobalStyle/>
+    </ThemeProvider>
   </StrictMode>,
 )
