36edd009a14669a07b61aba856b18f0a6d28c263
Azafman <0000880867@senaimgaluno.com.br>
2024-09-24 21:37:10 -0300

    Estrutura visual - tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
+import { PriceHighlight, TransactionContainer, TransactionsTable } from "./styles"
 
 export const Transactions = () => {
   return (
     <div>
-        <Header></Header>
-        <Summary></Summary>
+      <Header/>
+      <Summary/>
+
+        
+      <TransactionContainer>
+        <TransactionsTable>
+        <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td >Hambúrguer</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  -R$ 59,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionContainer>
     </div>
   )
 }
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const TransactionContainer = styled.main`
+    width: 100%;
+    max-width: 1120px;
+    margin: 4rem auto 0;//top horizontal bottom
+    padding: 0;
+`
+
+export const TransactionsTable = styled.table`
+    width: 100%;
+    border-collapse: separate;
+    border-spacing: 0 0.5rem;
+    //juntamente com o border-collpase faz com que aja espaçamento vertical entre as linhas da tabela
+    /* margin-top: 1.5rem; */
+
+    td {
+        padding: 1.25rem 2rem;
+        background-color: ${props => props.theme['gray-700']};
+
+        &:first-child {
+            border-top-left-radius: 6px;
+            border-bottom-left-radius: 6px;
+            width: 50%;
+        }
+
+        &:last-child {
+            border-top-right-radius: 6px;
+            border-bottom-right-radius: 6px;
+        }
+    }
+`
+interface PriceHighlightProps {
+    variant: 'income' | 'outcome'
+}
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+    color: ${props => props.variant === 'income' ? props.theme['green-300'] : props.theme['red-300']};
+`
