2e7fac46f86bceaa08d4e540183b9ca0dc310cf7
Azafman <0000880867@senaimgaluno.com.br>
2024-10-11 09:01:33 -0300

    Conexão com API - calculando resumo de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,35 +1,56 @@
+import { useContext } from 'react'
 import { SummaryCard, SummaryContainer } from './styles'
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from '@phosphor-icons/react'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
 
 export const Summary = () => {
-  return (
-    <SummaryContainer>
-        <SummaryCard>
-            <header>
-                <span>Entrada</span>
-                <ArrowCircleUp size={32} color="#00b37e"/>
-            </header>
+    const { transactions } = useContext(TransactionsContext);
+    const summary = transactions.reduce(
+        (acc, transaction) => {
+          if (transaction.type === 'income') {
+            acc.income += transaction.price;
+            acc.total += transaction.price;
+          } else {
+            acc.outcome += transaction.price;
+            acc.total -= transaction.price;
+          }
+          return acc;
+        },
+        {
+          income: 0,
+          outcome: 0,
+          total: 0,
+        },
+      );
+    
+    return (
+        <SummaryContainer>
+            <SummaryCard>
+                <header>
+                    <span>Entrada</span>
+                    <ArrowCircleUp size={32} color="#00b37e" />
+                </header>
 
-            <strong>R$ 17.450,00</strong>
-        </SummaryCard>
+                <strong>{summary.income}</strong>
+            </SummaryCard>
 
-        <SummaryCard>
-            <header>
-                <span>Entrada</span>
-                <ArrowCircleDown size={32} color="#f75a68"/>
-            </header>
+            <SummaryCard>
+                <header>
+                    <span>Saída</span>
+                    <ArrowCircleDown size={32} color="#f75a68" />
+                </header>
 
-            <strong>R$ 17.450,00</strong>
-        </SummaryCard>
+                <strong>{summary.outcome}</strong>
+            </SummaryCard>
 
-        <SummaryCard variant='green'>
-            <header>
-                <span>Entrada</span>
-                <CurrencyDollar size={32} color="#fff"/>
-            </header>
+            <SummaryCard variant='green'>
+                <header>
+                    <span>Total</span>
+                    <CurrencyDollar size={32} color="#fff" />
+                </header>
 
-            <strong>R$ 17.450,00</strong>
-        </SummaryCard>
-    </SummaryContainer>
-  )
+                <strong>{summary.total}</strong>
+            </SummaryCard>
+        </SummaryContainer>
+    )
 }
