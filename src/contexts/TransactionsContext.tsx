cd18f867dc83f5faad2fcf499dfe1cafe15620c2
Azafman <0000880867@senaimgaluno.com.br>
2024-10-19 10:41:39 -0300

    Conexão com a API - configurando Axios

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { TransactionType } from "../components/NewTransactionModal/styles";
+import { api } from "../libs/axios";
 
 interface TransactionType {
     id: string;
@@ -20,17 +21,15 @@ export const TrasanctionContextProvider = ({ children }: { children: ReactNode }
     const [transactions, setTransactions] = useState<TransactionType[]>([]);
     //quando criamos states, sempre é importate tipar o mesmo, principalmente quando se trata de objeto ou array.
     async function loadTransactions(query?: string) {
-        const url = new URL('http://localhost:3333/transactions');
-
-        if (query) {
-            url.searchParams.append('q', query);
-            //http://localhost:3333/transactions?q={query}
-        }
-        console.log(url)
-
-        const data = await fetch(url).then(response => response.json());
+        //"/transactions" ou "transactions" é a mesma coisa
+        const response = await api.get('/transactions', {
+            params: {
+                description: query
+            }
+        })//não preciso converter o json para js, a própria lib faz isso para mim
+        //console.log(response); esse objecto é semelhate a quando usamos fetch e then sem antes de dar um .json
         
-        setTransactions(data);
+        setTransactions(response.data);
     }
     useEffect(() => {
         loadTransactions();
